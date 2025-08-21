0320a9814885a8383694ad76c62d92f345a03b8b
Izzumi555 <izzumi555@gmail.com>
2025-08-21 04:44:11 +0200

    Forgot about git... Fixed some bugs

--- script.py
+++ script.py
@@ -15,39 +15,35 @@ class Tamagotchi:
         self.sick = False
         self.coins = 0
 
-    def stats(self):
-        print(f"Stats for {self.name}: ")
-        print(f"Name: {self.name}")
-        print(f"Hunger: {self.hunger}")
-        print(f"Dirty: {self.dirty}")
-        print(f"Health: {self.health}")
-        print(f"Energy: {self.energy}")
-        print(f"Coins: {self.money}")
+    def __repr__(self):
+        return(f"Stats for {self.name}: \nName: {self.name} \nHunger: {self.hunger} \nDirty: {self.dirty} \nHealth: {self.health} \nEnergy: {self.energy} \nCoins: {self.coins}")
 
     def needs(self):
         if self.health >= 50 and self.hunger >= 50 and self.energy >= 50:
             return f"Everything is fine with {self.name}!!"
-        elif self.health >= 50 and self.hunger >= 50 and self.energy <= 50:
-            return f"You should go play with {self.name}!"
-        elif self.health >= 50 and self.hunger <= 50 and self.energy <= 50:
-            return f"You should feed and play with {self.name}!!"
+        elif self.hunger < 50 and self.energy >= 50:
+            return f"{self.name} is hungry! Feed them!"
+        elif self.energy < 50 and self.hunger >= 50:
+            return f"{self.name} needs to rest or play!"
+        elif self.health < 50:
+            return f"{self.name} is sick! Heal them!"
+        else:
+            return f"{self.name} needs attention!"
 
     def feed(self):
         if self.hunger <= 90:
             print(f"You fed {self.name}.")
             print(f"Hunger +10")
             self.hunger += 10
-        elif self.hunger >= 90:
-            print(f"{self.name} is not hungry right now.")
         else:
-            print(f"You forgot to fed {self.name}..")
+            print(f"{self.name} is not hungry right now.")
             self.alive = False
 
     def clean(self):
-        if self.dirty <= 99:
+        if self.dirty < 75:
             print(f"You cleaned {self.name}.")
             self.dirty = 0
-        elif self.dirty >= 75:
+        elif self.dirty <= 99:
             print(f"You've cleaned {self.name} but it got sick!!")
             print(f"You should do something fast!!!")
             self.dirty = 0
@@ -56,7 +52,7 @@ class Tamagotchi:
             print(f"{self.name} died because it got way too sick ;((")
             self.alive = False
 
-    def health(self):
+    def heal(self):
         if self.sick:
             print(f"You've healed {self.name}.")
             print(f"Health +15")
@@ -73,18 +69,15 @@ class Tamagotchi:
         self.light = False
 
     def sleep(self):
-        if self.light == False and self.energy <= 65:
-            print(f"You put {self.name} to sleep")
-            print("Hunger -25")
-            print("Energy +25")
-            print("Dirty +15")
-            self.hunger -= 25
-            self.energy += 25
-            self.dirty += 15
-        elif self.light == False and self.energy > 65:
-            print(f"You should play first with {self.name} it have way to much energy!!!")
+        if not self.light and self.energy <= 65:
+            print(f"You put {self.name} to sleep.")
+            self.hunger = max(0, self.hunger - 25)
+            self.energy = min(100, self.energy + 25)
+            self.dirty = min(100, self.dirty + 15)
+        elif not self.light:
+            print(f"{self.name} has too much energy to sleep right now!")
         else:
-            print(f"{self.name} can't go to sleep beacuse lights are turn on!!!")
+            print(f"{self.name} can't sleep while the lights are on!")
 
     def play(self):
         if self.energy >= 20:
@@ -95,8 +88,8 @@ class Tamagotchi:
             print(f"{self.name} is tired right now.")
 
 class MiniGames:
-    def __init__(self):
-        self.coins = 0
+    def __init__(self, pet):
+        self.pet = pet
 
     def numberGuessing(self):
         number = random.randint(1, 10)
@@ -104,23 +97,34 @@ class MiniGames:
         try:
             if guess == number:
                 print("Correct!!")
-                print("Coins +5")
-                self.coins += 5
+                print("Coins +3")
+                time.sleep(2)
+                self.pet.coins += 5
             else:
                 print("Incorrect.")
+                time.sleep(2)
         except ValueError:
             print("Enter a number in a range 1 to 10.")
 
     def coinFlip(self):
-        pass
+        flip = random.choice(["HEADS", "TAILS"])
+        guess = input("Enter (heads / tails): ").upper()
+        if guess == flip:
+            print("Correct.")
+            print("Coins +1")
+            time.sleep(2)
+            self.pet.coins += 1
+        else:
+            print("Incorrect.")
+            time.sleep(2)
 
 class Game:
     def __init__(self):
         self.pet = None
 
     def clearScreen(self):
-        sys.stdout.write("\0333[H\0333[J")
-        sys.stdout.flush
+        sys.stdout.write("\033[H\033[J")
+        sys.stdout.flush()
 
     def pause(self, seconds=5):
         time.sleep(seconds)
@@ -128,54 +132,70 @@ class Game:
     def main(self):
         self.clearScreen()
         print("====== Tamagotchi ======")
-        if self.pet == None:
-            self.pet_name = input("Enter your pets name: ")
-            self.pet = Tamagotchi(self.pet_name)
-        else:
-            while True:
-                if self.pet == None:
-                    self.pet_name = input("Enter your pets name: ")
-                    self.pet = Tamagotchi(self.pet_name)
-                else:
-                    self.clear_screen()
-                    print("=== MENU ===")
-                    print("1) Show stats")
-                    print("2) Show needs")
-                    print("3) Feed")
-                    print("4) Play")
-                    print("5) Quit")
+        while True:
+            if self.pet == None:
+                self.pet_name = input("Enter your pets name: ")
+                self.pet = Tamagotchi(self.pet_name)
+            else:
+                self.clearScreen()
+                print("=== MENU ===")
+                print("1) Show stats")
+                print("2) Show needs")
+                print("3) Feed")
+                print("4) Play")
+                print("5) MiniGames")
+                print("6) Quit")
+    
+                try:
+                    choice = int(input("$ "))
+                except ValueError:
+                    print("\nInvalid choice. Please enter a number.")
+                    self.pause(2)
+                    continue
 
+                if choice == 1:
+                    self.clearScreen()
+                    print(self.pet)
+                    self.pause()
+                elif choice == 2:
+                    self.clearScreen()
+                    print(self.pet.needs())
+                    self.pause(3)
+                elif choice == 3:
+                    self.clearScreen()
+                    self.pet.feed()
+                    self.pause(3)
+                elif choice == 4:
+                    self.clearScreen()
+                    self.pet.play()
+                    self.pause(3)
+                elif choice == 5:
+                    self.clearScreen()
+                    print("1) Pick a number")
+                    print("2) Flip a coin")
                     try:
                         choice = int(input("$ "))
                     except ValueError:
                         print("\nInvalid choice. Please enter a number.")
                         self.pause(2)
                         continue
-
                     if choice == 1:
-                        self.clear_screen()
-                        print(self.pet)
-                        self.pause()
+                        self.clearScreen
+                        MiniGames(self.pet).numberGuessing()
                     elif choice == 2:
-                        self.clear_screen()
-                        print(self.pet.needs())
-                        self.pause(3)
-                    elif choice == 3:
-                        self.clear_screen()
-                        self.pet.feed()
-                        self.pause(3)
-                    elif choice == 4:
-                        self.clear_screen()
-                        self.pet.play()
-                        self.pause(3)
-                    elif choice == 5:
-                        self.clear_screen()
-                        print(f"Goodbye! {self.pet.name} will miss you!")
-                        self.pause(1.5)
-                        break
+                        self.clearScreen
+                        MiniGames(self.pet).coinFlip()
                     else:
-                        print("\nInvalid option, please choose between 1 and 5.")
+                        print("Invalid option, please choose between 1 and 2.")
                         self.pause()
+                elif choice == 6:
+                    self.clearScreen()
+                    print(f"Goodbye! {self.pet.name} will miss you!")
+                    self.pause(1.5)
+                    break
+                else:
+                    print("\nInvalid option, please choose between 1 and 5.")
+                    self.pause()
 
 
 if __name__ == '__main__':
